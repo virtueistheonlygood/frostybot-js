ab931d11e0bce1188a97f3f760bae502d25d17cc
CryptoMF <cryptomf@frostytrading.com>
2021-02-14 14:47:12 +0200

    Improved signal processing and tracking

--- database/updates/update.mysql.1.sql
+++ database/updates/update.mysql.1.sql
@@ -1,19 +1,3 @@
--- Version 0
-
---
--- Create logs table
---
-
-CREATE TABLE IF NOT EXISTS `logs` (
-  `uid` int unsigned NOT NULL AUTO_INCREMENT,
-  `uuid` char(36) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000',
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `type` varchar(10) NOT NULL,
-  `message` text NOT NULL,
-  PRIMARY KEY (`uid`),
-  KEY `IDX_UUID_TS` (`uuid`,`timestamp`)
-) ENGINE=InnoDB AUTO_INCREMENT=9434 DEFAULT CHARSET=latin1;
-
 -- Version 1
 
 --
